ac9fa42a1b637ccfd214265cafb765b5aee5700f
Rohit <rohit.verma@itsrizzoli.it>
2024-10-10 14:45:21 +0200

    Added ViewModel and Modified XAML

---
+++ UserRegistry/ViewModels/UserViewModel.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+using System.ComponentModel;
+using UserRegistry.Models;
+
+namespace UserRegistry.ViewModels
+{
+    internal class UserViewModel : INotifyPropertyChanged
+    {
+        private User _user = new("", "");
+        public ObservableCollection<User> Users { get; set; } = UserManager.GetUsers();
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public User GetUser
+        {
+            get => _user;
+            set
+            {
+                _user = value;
+                OnPropertyChanged(nameof(Name));
+                OnPropertyChanged(nameof(Surname));
+                OnPropertyChanged(nameof(DateOfBirth));
+                OnPropertyChanged(nameof(Address));
+                OnPropertyChanged(nameof(City));
+                OnPropertyChanged(nameof(Cap));
+                OnPropertyChanged(nameof(PhoneNumber));
+            }
+        }
+
+        public string Name
+        {
+            get => _user.Name;
+            set
+            {
+                _user.Name = value;
+                OnPropertyChanged(nameof(Name));
+            }
+        }
+
+        public string Surname
+        {
+            get => _user.Surname;
+            set
+            {
+                _user.Surname = value;
+                OnPropertyChanged(nameof(Surname));
+            }
+        }
+
+        public DateTime DateOfBirth
+        {
+            get => _user.DateOfBirth;
+            set
+            {
+                _user.DateOfBirth = value;
+                OnPropertyChanged(nameof(DateOfBirth));
+            }
+        }
+
+        public string Address
+        {
+            get => _user.Address;
+            set
+            {
+                _user.Address = value;
+                OnPropertyChanged(nameof(Address));
+            }
+        }
+
+        public string City
+        {
+            get => _user.City;
+            set
+            {
+                _user.City = value;
+                OnPropertyChanged(nameof(City));
+            }
+        }
+
+        public int Cap
+        {
+            get => _user.Cap;
+            set
+            {
+                _user.Cap = value;
+                OnPropertyChanged(nameof(Cap));
+            }
+        }
+
+        public int PhoneNumber
+        {
+            get => _user.PhoneNumber;
+            set
+            {
+                _user.PhoneNumber = value;
+                OnPropertyChanged(nameof(PhoneNumber));
+            }
+        }
+
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
--- UserRegistry/Views/Login.xaml.cs
+++ UserRegistry/Views/Login.xaml.cs
@@ -12,19 +12,20 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using UserRegistry.Models;
+using UserRegistry.ViewModels;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
 
 namespace UserRegistry.Views
 {
-    /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
+   
     public sealed partial class Login : Page
     {
         public Login()
         {
             this.InitializeComponent();
+            DataContext = new UserViewModel();
         }
     }
 }
