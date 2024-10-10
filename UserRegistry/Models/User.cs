3627a00c53e22d9be29993107d5330efa0609a27
Rohit <rohit.verma@itsrizzoli.it>
2024-10-10 23:37:44 +0200

    Updated DOB type and got rid off UserManager

--- UserRegistry/Models/User.cs
+++ UserRegistry/Models/User.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.ComponentModel.DataAnnotations;
+using System.Numerics;
 
 namespace UserRegistry.Models
 {
@@ -10,11 +11,12 @@ namespace UserRegistry.Models
         [Key] public int UserId { get; private set;}
         [Required] public string Name { get; set; }
         [Required] public string Surname { get; set; }
-        public DateTime DateOfBirth { get; set; }
+        public DateTimeOffset DateOfBirth { get; set; }
+        public string DateOfBirthDisplay => DateOfBirth.ToString("dd-MM-yyyy");
         public string Address { get; set; }
         public string City { get; set; }
         public int Cap { get; set; }
-        public int PhoneNumber { get; set; }
+        public string PhoneNumber { get; set; }
 
         public User()
         {
