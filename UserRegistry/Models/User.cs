5736acfe187c946f55020e7b71032b9f739d5726
Rohit <rohit.verma@itsrizzoli.it>
2024-10-10 11:42:58 +0200

    Initial Updates

---
+++ UserRegistry/Models/User.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace UserRegistry.Models
+{
+    internal class User
+    {
+        private static int _commonIdCounter;
+
+        [Key] public int UserId { get; private set; }
+        [Required] public string Name { get; set; }
+        [Required] public string Surname { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public int Cap { get; set; }
+        public int PhoneNumber { get; set; }
+
+        public User(string name, string surname)
+        {
+            _commonIdCounter++;
+            UserId = _commonIdCounter;
+            Name = name;
+            Surname = surname;
+        }
+    }
+}
---
+++ UserRegistry/Models/UserManager.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserRegistry.Models
+{
+    internal class UserManager
+    {
+        public static ObservableCollection<User> UserList = [];
+
+        public ObservableCollection<User> GetUsers()
+        {
+            return UserList;
+        }
+
+        public void AddUsers(User user)
+        {
+            UserList.Add(user);
+        }
+    }
+}
